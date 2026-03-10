98a72c9a0e42bf553383c70ee21083cd4680f4e8
nhanlt <nhanlt@smartosc.com>
2026-03-10 10:18:27 +0700

    use rxResource

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,12 +1,22 @@
-import { httpResource } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
 import { Product } from './product';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private productsUrl = 'api/products-2';
+  private http = inject(HttpClient);
+
   selectedProduct = signal<Product | undefined>(undefined);
-  productResource = httpResource<Product[]>(() => this.productsUrl, { defaultValue: [] });
+  // productResource = httpResource<Product[]>(() => this.productsUrl, { defaultValue: [] });
+  productResource = rxResource({
+    stream: () => this.http.get<Product[]>(this.productsUrl).pipe(
+      map(items => items.sort((a, b) => a.productName > b.productName ? 1 : -1))
+    ),
+    defaultValue: []
+  });
 }
